78cd61724c526f4ba896ac2a7aad7269d27a6e1b
Farmpasinee <52791928+Farmpasinee@users.noreply.github.com>
2019-10-03 09:32:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def callback():
     userText = decoded['queryResult']['intent']['displayName']
    if (userText == 'สวัสดี') :
         sendText(user,'ดีจ่ะ')
-    elif (userText == 'ไอ้บ้า') :
+   elif (userText == 'ไอ้บ้า') :
         sendText(user,'อืม')
-    else :
+   else :
         sendText(user,'อ่า')
-    return '',200
+   return '',200
 
 
 def sendText(user, text):
